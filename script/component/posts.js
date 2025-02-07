79ecb6d63fa4fbc0c3487d8492f9463224961221
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-02-07 13:23:57 +0100

    add some basic js to posts.js component

--- script/component/posts.js
+++ script/component/posts.js
@@ -1,3 +1,9 @@
-function renderPosts() { }
+function renderPosts() {
+    const postArea = document.getElementById("post-area")
+    const post = document.createElement("article")
+
+    postArea.append(post);
+
+}
 
 function renderSinglePost() { }
