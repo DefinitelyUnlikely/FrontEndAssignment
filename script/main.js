7957fecaa77aae525d28efba5784491f6aad99c5
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-02-07 09:04:55 +0100

    Add main and footer section

--- script/main.js
+++ script/main.js
@@ -1,8 +1,14 @@
 const hamMenu = document.querySelector(".ham-menu");
-
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
 });
+
+let testing = "";
+let testDiv = document.getElementById("test");
+
+testDiv.innerText = "Testar";
+
+let main = document.getElementsByTagName("main");
