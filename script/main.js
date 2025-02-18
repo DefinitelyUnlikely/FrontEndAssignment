8c4e8233c7d6dbd6908dbc96b47af5e436f09f7c
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-02-18 08:44:08 +0100

    Add basic create-post box

--- script/main.js
+++ script/main.js
@@ -14,18 +14,31 @@ hamMenu.addEventListener("click", (event) => {
     event.stopPropagation();
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
-});
 
-offScreenMenu.addEventListener("click", (event) => { event.stopPropagation(); })
+    offScreenMenu.addEventListener("click", (event) => { event.stopPropagation(); })
 
-document.addEventListener("click", () => {
-    if (offScreenMenu.classList.contains("active")) {
-        hamMenu.classList.toggle("active");
-        offScreenMenu.classList.toggle("active");
-    }
+    document.addEventListener("click", () => {
+        if (offScreenMenu.classList.contains("active")) {
+            hamMenu.classList.toggle("active");
+            offScreenMenu.classList.toggle("active");
+        }
+    });
 });
 
 // Search box JavaScript
 // Nothing here yet
+
+
+// Create Post JavaScript
+const createButton = document.getElementById("create-post");
+const createArea = document.getElementById("create-post-area");
+createButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    createArea.classList.remove("hidden");
+
+    createArea.addEventListener("click", (event) => { event.stopPropagation(); })
+
+    document.addEventListener("click", () => { createArea.classList.add("hidden"); });
+})
 
 main();
