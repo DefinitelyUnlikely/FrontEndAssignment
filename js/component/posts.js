7d4349908fb6161498376de63546193aa02e9daf
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-02-22 21:41:18 +0100

    Add buttons

--- js/component/posts.js
+++ js/component/posts.js
@@ -8,7 +8,35 @@ export async function renderAllPosts() {
     let postArray = await getAllPosts();
 
     for (let post of postArray.posts) {
-        console.log(post)
+        const renderedPost = document.createElement("article");
+
+        const title = document.createElement("h3");
+        const body = document.createElement("p");
+
+        const bottomDiv = document.createElement("div");
+        const leftBottom = document.createElement("div");
+        const rightBottom = document.createElement("div");
+        const upvote = document.createElement("button");
+        const downvote = document.createElement("button");
+        const share = document.createElement("button");
+
+        title.innerText = post.title;
+        body.innerText = post.body.slice(0, 79) + "..."
+        upvote.innerText = "Like";
+        downvote.innerText = "Dislike";
+        share.innerText = "Share";
+
+        postArea.append(renderedPost)
+        renderedPost.append(title);
+        renderedPost.append(body);
+        renderedPost.append(bottomDiv);
+
+        bottomDiv.append(leftBottom);
+        bottomDiv.append(rightBottom);
+        leftBottom.append(upvote);
+        leftBottom.append(downvote);
+        rightBottom.append(share);
+
     }
 }
 
