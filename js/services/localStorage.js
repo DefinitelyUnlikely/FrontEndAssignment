0c761496adc0f123cc846bed9bed21a4d6ee3913
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-02-28 10:42:30 +0100

    add json.stringify to arrays

--- js/services/localStorage.js
+++ js/services/localStorage.js
@@ -1,1 +1,38 @@
 // File for all localStorage related data.
+
+export function getPostData() {
+    return [];
+}
+
+export function savePostData(postArray) {
+    try {
+        localStorage.setItem("posts", JSON.stringify(postArray));
+    } catch (e) {
+        throw new Error("Could not save post data");
+    }
+}
+
+export function getCommentData() {
+    return [];
+
+}
+
+export function saveCommentData(commentArray) {
+    try {
+        localStorage.setItem("comments", JSON.stringify(commentArray));
+    } catch (e) {
+        throw new Error("Could not save comment data");
+    }
+}
+
+export function getUserData() {
+    return [];
+}
+
+export function saveUserData(userArray) {
+    try {
+        localStorage.setItem("users", JSON.stringify(userArray));
+    } catch (e) {
+        throw new Error("Could not save user data");
+    }
+}
