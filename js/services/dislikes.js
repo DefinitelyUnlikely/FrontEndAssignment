4e99953d33a29da21ea3a4b64fbda578fae52119
DefinitelyUnlikely <Martin@kaarjohansson.se>
2025-03-15 16:02:58 +0100

    Update functions with new methodology

--- js/services/dislikes.js
+++ js/services/dislikes.js
@@ -1,5 +1,9 @@
 import { getSinglePost } from "../data/posts.js";
 
-export async function changePostDislikes(postId, remove = false) {
+export async function changePostDislikes(postId) {
     let post = await getSinglePost(postId);
+}
+
+export function getUserPostDislike(postId, userId) {
+    return localStorage.getItem(`dislikes/posts/${postId}/${userId}`)
 }
--- js/services/likes.js
+++ js/services/likes.js
@@ -1,13 +1,53 @@
+import { getCurrentlySelectedUser } from "../constants.js";
 import { getCommentsByPost } from "../data/comments.js";
 import { getSinglePost } from "../data/posts.js";
+import { getUserPostDislike } from "./dislikes.js";
 
-export async function changePostLikes(postId, remove = false) {
-    let post = await getSinglePost(postId);
 
+
+export async function changePostLikes(postId) {
+    let user = getCurrentlySelectedUser();
+    let post = getSinglePost(postId);
+
+    if (!user) {
+        console.log("Please log in");
+        return;
+    }
+
+    let like = getUserPostLike(postId, user.id);
+    let dislike = getUserPostDislike(postId, user.id);
+
+    if (!like && !dislike) {
+        // Also need to save this to localStorage
+        post.reactions.likes += 1;
+        localStorage.setItem(`likes/posts/${postId}/${user.id}`, "true")
+        return;
+    }
+
+    if (!like && dislike) {
+        // Need to save updated reactions to localStorage.
+        post.reactions.likes += 1;
+        post.reactions.dislikes -= 1;
+        localStorage.removeItem(`dislikes/posts/${postId}/${user.id}`)
+        return;
+    }
+
+    // At this point, they've already liked the post and we undo the like.
+    post.reactions.likes -= 1;
+    localStorage.removeItem(`likes/posts/${postId}/${user.id}`)
 
 }
 
-export async function changeCommentLikes(postId, commentId, remove = false) {
+export async function changeCommentLikes(postId, commentId) {
     let comments = await getCommentsByPost(postId);
 
+}
+
+
+export function getUserPostLike(postId, userId) {
+    return localStorage.getItem(`likes/posts/${postId}/${userId}`)
+}
+
+export function getUserCommentLike(commentId, userId) {
+    return localStorage.getItem(`likes/comments/${commentId}/${userId}`)
 }
