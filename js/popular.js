62f3385f7bf851b841fee143fe0b83b328cba57e
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-03-17 13:22:19 +0100

    Fix case sensitive file name

--- js/popular.js
+++ js/popular.js
@@ -1,7 +1,7 @@
 import { renderPosts } from "./component/posts.js";
 import { hamburgerMenu } from "./component/hamburger.js"
 import { createPostButton } from "./component/createPost.js";
-import { OffScreenMenu } from "./component/offscreenmenu.js";
+import { OffScreenMenu } from "./component/offScreenMenu.js";
 import { getAllPosts } from "./data/posts.js";
 import { searchBar } from "./component/search.js";
 
